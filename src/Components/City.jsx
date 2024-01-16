7a73cec1cd99c25ad8d0b18667400e6cec8efb2b
Hemanth Kumar S <hemanthkumars0499@gmail.com>
2024-01-16 21:10:36 +0530

    Fix 404 error

--- src/Components/City.jsx
+++ src/Components/City.jsx
@@ -5,14 +5,14 @@ import { useCities } from "../contexts/CitiesContext";
 import Spinner from "./Spinner";
 import BackButton from "./BackButton";
 
-const flagemojiToPNG = (flag) => {
-  var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
-    .map((char) => String.fromCharCode(char - 127397).toLowerCase())
-    .join("");
-  return (
-    <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
-  );
-};
+// const flagemojiToPNG = (flag) => {
+//   var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt())
+//     .map((char) => String.fromCharCode(char - 127397).toLowerCase())
+//     .join("");
+//   return (
+//     <img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt="flag" />
+//   );
+// };
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
